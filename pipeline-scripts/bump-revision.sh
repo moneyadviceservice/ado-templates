d4aa9b0260204256cf603d69e5e4d68998e0dbda
Zoe Cope <zoe.cope@hmcts.net>
2025-04-16 16:41:02 +0100

    add scripts

---
+++ pipeline-scripts/bump-revision.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git config --global user.email "NotificationBot@maps.org.uk"
+git config --global user.name "Notification Bot"
+
+cd shared-infrastructure/components/apim
+current_revision=$(grep 'revision' $TF_FILE | awk -F'= ' '{print $2}' | tr -d '"')
+
+next_revision=$((current_revision + 1))
+echo "Current API Revision: $current_revision"
+echo "Next API Revision: $next_revision"
+
+awk -v new_revision="$next_revision" '/revision[[:space:]]*=/ {sub(/"[^"]*"/, "\"" new_revision "\"")} 1' $TF_FILE > temp && mv temp $TF_FILE
+
+git pull origin $BRANCH_NAME
+git checkout -b update-spec
+git add $TF_FILE
+git commit -m "Bump API revision to $next_revision"
+git push https://$PAT_TOKEN@dev.azure.com/moneyandpensionsservice/MaPS%20Digital/_git/$AZURE_REPO HEAD:refs/heads/update-spec
+az repos pr create --repository "$AZURE_REPO" --target-branch develop --source-branch update-spec --title "Update OpenAPI spec" --bypass-policy true --delete-source-branch true --draft false
---
+++ pipeline-scripts/commit-specs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TARGET_DIR="$SERVICE_NAME/wwwroot/swagger/v1/"
+PAT_TOKEN="${PAT_TOKEN}"
+BRANCH_NAME="${BRANCH_NAME}"
+SERVICE_NAME="${SERVICE_NAME}"
+SERVICE_PATH="${SERVICE_PATH}"
+echo "TARGET_DIR: $SERVICE_NAME/wwwroot/swagger/v1/"
+echo "BRANCH_NAME: $BRANCH_NAME"
+echo "SERVICE_NAME: $SERVICE_NAME"
+echo "SERVICE_PATH: $SERVICE_PATH"
+
+cd $SERVICE_PATH/$SERVICE_NAME/app/$SERVICE_NAME
+
+dotnet tool list -g
+dotnet tool restore
+dotnet restore
+dotnet build
+
+# Check for changes
+if [[ -n $(git status --porcelain "$TARGET_DIR") ]]; then
+    echo "🔍 Changes detected in $TARGET_DIR. Committing..."
+
+    # Configure Git
+    git config user.email "NotificationBot@maps.org.uk"
+    git config user.name "Notification Bot"
+
+    # Commit and push changes
+    git add $SERVICE_NAME/wwwroot/swagger/v1/
+    git commit -m "Auto-update OpenAPI spec"
+    git push https://$PAT_TOKEN@dev.azure.com/moneyandpensionsservice/MaPS%20Digital/_git/$AZURE_REPO HEAD:refs/heads/$BRANCH_NAME
+else
+    echo "✅ No changes detected. Skipping commit."
+fi

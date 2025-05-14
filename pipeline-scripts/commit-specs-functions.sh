2edc2168137cd47d112180b0a892620e89d44285
Zoe Cope <zoe.cope@hmcts.net>
2025-05-14 09:21:37 +0100

    amend functions commit specs

--- pipeline-scripts/commit-specs-functions.sh
+++ pipeline-scripts/commit-specs-functions.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "SPEC_FILE_NAME: $SPEC_FILE_NAME"
-echo "TARGET_DIR: wwwroot/swagger/v1/$SPEC_FILE_NAME"
+echo "TARGET_DIR: $SERVICE_PATH/app/$FUNCTION_NAME/wwwroot/swagger/v1/$SPEC_FILE_NAME"
 echo "BRANCH_NAME: $BRANCH_NAME"
 # echo "SERVICE_NAME: $SERVICE_NAME"
-# echo "SERVICE_PATH: $SERVICE_PATH"
+SERVICE_PATH="${SERVICE_PATH}"
 echo "FUNCTION_NAME: $FUNCTION_NAME"
 
-TARGET_DIR="wwwroot/swagger/v1/$SPEC_FILE_NAME"
+TARGET_DIR="$SERVICE_PATH/app/$FUNCTION_NAME/wwwroot/swagger/v1/$SPEC_FILE_NAME"
 
 export AzureWebJobsStorage="UseDevelopmentStorage=true"
 export FUNCTIONS_WORKER_RUNTIME="dotnet-isolated"
 export FUNCTIONS_INPROC_NET8_ENABLED="true"
 
-cd app/$FUNCTION_NAME
+cd $SERVICE_PATH/app/$FUNCTION_NAME
 
 npm i -g azure-functions-core-tools@4 --unsafe-perm true
 pwd
